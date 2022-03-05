a19a48c9ca556a250bc9b9a23f4b08aa11ee48c5
Phil McGarty <philmcgarty@hotmail.com>
2022-03-04 22:06:39 -0500

    added first few user prompts and character variables

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,41 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
+//THIS IS THE FUNCTION TO CHANGE
 var generatePassword = function() {
-  //return "hello";
+  var lCase = "abcdefghijklmnopqrstuvwxyz";
+  var uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var nums = "0123456789";
+  var specials = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~]"+'"';
+  var selectedChars = "";
+
   //Confirm password length
   var passLength = window.prompt("How many characters in password? (8-128 only)");
   console.log(passLength);
+  
+  //Confirm lowercase
+  var lCaseConfirm = window.confirm("Should the password contain lowercase letters? e.g. a-z");
+  if (lCaseConfirm) {
+    selectedChars += lCase;
+    console.log(selectedChars); 
+  }
+  //Confirm
+  var uCaseConfirm = window.confirm("Should the password contain uppercase letters? e.g A-Z");
+  if (uCaseConfirm) {
+    selectedChars += uCase;
+    console.log(selectedChars);
+  }
 
 }
+//DON'T CHANGE ANYTHING BELOW THIS!
+
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
@@ -40,3 +61,6 @@ generateBtn.addEventListener("click", writePassword);
 //e.g a-z, A-Z, 0-9, [specials -  !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]
 //add each string to a master variable that contains all available characters for password
 //if loop that randomly selects from available characters to length stated by user
+//select a random character each time from the available string using - chatAt(random number from string)
+
+//**If time, create function for character confirmations to reduce code**
